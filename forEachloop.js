00a717724d5c88e9888a19eb843df77b8556f5ef
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-12-30 12:30:11 +0500

    Add files via upload

---
+++ forEachloop.js
@@ -0,0 +1,23 @@
+/ForEach Loop
+
+<script>
+const abc = [
+    {id:10,
+        texx:'Want to see you',
+        context:true
+    },
+    {
+        id:20,
+        texx:'Really',
+        context:true
+    },
+    {
+        id:30,
+        texx:'YEPp',
+        context:true
+    }
+];
+abc.forEach(function(abc){
+    console.log(abc.texx);
+});
+    </script>
